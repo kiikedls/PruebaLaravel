98c6cc2166a8a374ab167ca6e642820806ad8319
kiikedls <jespinozadelossatnos@gmail.com>
2019-02-25 20:01:36 -0600

    version 2

--- resources/views/kiike.blade.php
+++ resources/views/kiike.blade.php
@@ -81,11 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kiike_dls
                 </div>
 
                 <div class="links">
-                    <a href="/kiike">Documentation</a>
+                    <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
